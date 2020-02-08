793548d39d8ae6c905a7a189905cfd824a0d4501
yutarou12 <52701144+yutarou12@users.noreply.github.com>
2020-02-08 12:42:17 +0900

    インデントの修正

--- src/status/Main.php
+++ src/status/Main.php
@@ -17,29 +17,29 @@ use metowa1227\moneysystem\api\core\API;
 use status\Task\scoreTask;
 
 class Main extends PluginBase implements Listener{
-
+	
 	public function onEnable(){ 
 		date_default_timezone_set('Asia/Tokyo');
-
-       $this->getServer()->getPluginManager()->registerEvents($this,$this);
-           $this->getScheduler()->scheduleRepeatingTask(new scoreTask($this), 5);
+		
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getScheduler()->scheduleRepeatingTask(new scoreTask($this), 5);
 
 		$this->getLogger()->notice("StatusScoreBoard[MS版]を読み込みました。by  yutarou1241477");
 		$this->api = $this->getServer()->getPluginManager()->getPlugin("MoneySystem");
-        if($this->api == null){
-		$this->getLogger()->error("MoneySystemが見つかりません。サーバーを停止します。");
-		$this->getServer()->shutdown();
+		if($this->api == null){
+			$this->getLogger()->error("MoneySystemが見つかりません。サーバーを停止します。");
+			$this->getServer()->shutdown();
 		}else{
-		$this->getLogger()->info("MoneySystemを確認しました。");
+			$this->getLogger()->info("MoneySystemを確認しました。");
 		}
-
+		
 		$this->api = $this->getServer()->getPluginManager()->getPlugin("EasyScoreboardAPI");
-        if($this->api == null){
-		$this->getLogger()->error("EasyScoreboardAPIが見つかりません。サーバーを停止します。");
-		$this->getServer()->shutdown();
+		if($this->api == null){
+			$this->getLogger()->error("EasyScoreboardAPIが見つかりません。サーバーを停止します。");
+			$this->getServer()->shutdown();
 		}else{
-		$this->getLogger()->info("EasyScoreboardAPIを確認しました。");
+			$this->getLogger()->info("EasyScoreboardAPIを確認しました。");
 		}
-    }
+	}
 }
 
